6ea95301348f18120258474c12f453bb40e3c63d
paupalou <paupalou@gmail.com>
2022-03-28 23:26:03 +0200

    install fix & readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dots
 Your dotfiles companion
+
+## Install
+`bash -c "$(curl -s https://raw.githubusercontent.com/paupalou/dots/main/install.sh)"`
--- install.sh
+++ install.sh
@@ -97,8 +97,7 @@ _print_arguments
 
 if [ ! -d "$__destination_path" ]; then
   _print_installing
-  printf "$(_success) Dots installed, run %sdots to see options" "$(tput bold)$(tput setaf 6)"
-  _reset_to_normal
+  printf "$(_success) Dots installed, run %sdots%s to see options" "$(tput bold)$(tput setaf 6)" "$(_reset_to_normal)"
 else
   printf "$(_info) Dots its already installed in %s%s%s" "$(tput bold)$(tput setaf 6)" "$__destination_path" "$(_reset_to_normal)"
 fi
