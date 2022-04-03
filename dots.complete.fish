4a1645dccb9cb678ca3c2212fe42ff57b4425c9a
paupalou <paupalou@gmail.com>
2022-04-03 03:47:11 +0200

    improved fish completion

--- dots.complete.fish
+++ dots.complete.fish
@@ -6,17 +6,14 @@ complete --command dots --no-files --condition "__fish_seen_subcommand_from sync
 # Config
 complete --command dots --no-files --condition __fish_use_subcommand --arguments config -d "Prints dots user config"
 
-complete --command dots --no-files --condition "__fish_seen_subcommand_from config" -d "Create dots user config file"
-complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --short c
-complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --long create
+complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --arguments --create -d "Create dots user config file"
+complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --short c -d "Create dots user config file"
 
-complete --command dots --no-files --condition "__fish_seen_subcommand_from config" -d "Prints dots default config"
-complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --short d
-complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --long default
+complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --arguments --default -d "Prints dots default config"
+complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --short d -d "Prints dots default config"
 
-complete --command dots --no-files --condition "__fish_seen_subcommand_from config" -d "Open $EDITOR to edit dots config file"
-complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --short e
-complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --long edit
+complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --arguments --edit -d "Open $EDITOR to edit dots config file"
+complete --command dots --exclusive --condition "__fish_seen_subcommand_from config" --short e -d "Open $EDITOR to edit dots config file"
 
 # Version
 complete --command dots --no-files --condition __fish_use_subcommand --arguments version -d "Prints dots version"
